699e303699fde47ecf9509750294fad923efe099
mvl22 <martin@lucas-smith.co.uk>
2015-05-18 09:37:24 +0100

    Move MD5 check inside download block

--- run.sh
+++ run.sh
@@ -221,16 +221,16 @@ sudo -u ${username} mkdir -p data/${osmdatafolder}
 
 # Download OSM data if not already present
 if test ! -r ${osmdatapath}; then
-    echo "\n#\tDownload OSM data"
-    sudo -u ${username} wget --output-document=${osmdatapath}.md5 ${osmdataurl}.md5
-    sudo -u ${username} wget --output-document=${osmdatapath} ${osmdataurl}
-fi
-
-#	Check the md5 matches
-if [ "$(md5sum ${osmdatapath} | awk '{print $1;}')" != "$(cat ${osmdatapath}.md5 | awk '{print $1;}')" ]; then
-    echo "#\tThe md5 checksum for osmdatapath: ${osmdatapath} does not match, stopping."
-    exit 1
-    echo "\n#\tDownloaded OSM data integrity verified by md5 check."
+	echo "\n#\tDownload OSM data"
+	sudo -u ${username} wget --output-document=${osmdatapath} ${osmdataurl}
+	
+	# Verify with an MD5 match
+	sudo -u ${username} wget --output-document=${osmdatapath}.md5 ${osmdataurl}.md5
+	if [ "$(md5sum ${osmdatapath} | awk '{print $1;}')" != "$(cat ${osmdatapath}.md5 | awk '{print $1;}')" ]; then
+		echo "#\tThe md5 checksum for osmdatapath: ${osmdatapath} does not match, stopping."
+		exit 1
+		echo "\n#\tDownloaded OSM data integrity verified by md5 check."
+	fi
 fi
 
 
