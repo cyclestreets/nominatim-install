1c293c1c59a6094195c675580776f4e8dba9d86d
mtmail <mtmail@gmx.net>
2015-10-21 21:26:10 +0200

    fixed filename of Munin configuration
    The filename should be `nominatim_requests`

--- run.sh
+++ run.sh
@@ -195,7 +195,7 @@ service postgresql restart
 # !! Look at the comments at the top of the nominatim_importlag file in the following and copy the setup section to a new file in: /etc/munin/plugin-conf.d/
 ln -s '/home/nominatim/Nominatim/munin/nominatim_importlag' '/etc/munin/plugins/nominatim_importlag'
 ln -s '/home/nominatim/Nominatim/munin/nominatim_query_speed' '/etc/munin/plugins/nominatim_query_speed'
-ln -s '/home/nominatim/Nominatim/munin/nominatim_nominatim_requests' '/etc/munin/plugins/nominatim_nominatim_requests'
+ln -s '/home/nominatim/Nominatim/munin/nominatim_requests' '/etc/munin/plugins/nominatim_requests'
 
 
 # Needed to help postgres munin charts work
